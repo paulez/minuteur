9c21134b93e4f7a0389b4ca24d91acf673791029
Paul Ezvan <paul@ezvan.fr>
2021-02-21 22:22:01 +0100

    Add icon.

--- minuteur_fr_FR.ts
+++ minuteur_fr_FR.ts
@@ -5,19 +5,19 @@
     <name>Minuteur</name>
     <message>
         <location filename="minuteur.ui" line="20"/>
-        <location filename="minuteur_autogen/include/ui_minuteur.h" line="127"/>
+        <location filename="minuteur_autogen/include/ui_minuteur.h" line="131"/>
         <source>Minuteur</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="minuteur.ui" line="56"/>
-        <location filename="minuteur_autogen/include/ui_minuteur.h" line="128"/>
+        <location filename="minuteur.ui" line="60"/>
+        <location filename="minuteur_autogen/include/ui_minuteur.h" line="132"/>
         <source>HH:mm:ss</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="minuteur.ui" line="104"/>
-        <location filename="minuteur_autogen/include/ui_minuteur.h" line="132"/>
+        <location filename="minuteur.ui" line="108"/>
+        <location filename="minuteur_autogen/include/ui_minuteur.h" line="136"/>
         <source>00:00:00</source>
         <translation type="unfinished"></translation>
     </message>
